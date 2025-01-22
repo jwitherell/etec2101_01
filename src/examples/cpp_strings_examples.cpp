fe8e0919f301195d9a969cd638b89d3c98d8416c
Jason Witherell <jwitherell@shawnee.edu>
2025-01-22 14:04:57 -0500

    Work in class from 1/22/2025

--- src/examples/cpp_strings_examples.cpp
+++ src/examples/cpp_strings_examples.cpp
@@ -47,9 +47,28 @@ int main()
     (*pptr).set_hourly_rate(12.35f);        // Same as line 45, but uglier
 
     delete pptr;                            // Like free -- frees up the memory
-                                            //  pptr points to.
+                                            //  pptr points to.  The C++ runtime
+                                            //  calls the destructor right before
+                                            //  the memory is freed.
+    pptr = NULL;        // NULL is just a macro for 0
+    int y = NULL;       // valid, but "weird"
+    pptr = nullptr;     // A C++-only thing
+    //y = nullptr;        // Error b/c pointers are only ones to use nullptr
+
+    pptr = new Person(45, "Kim", "Young");
+    // Do things with it
+    delete pptr;
 
 
     std::cout << p.get_id() << " " << p.get_first_name() << "\n";
+
+    // p and q were allocated on the STACK (an area of memory).  STACK is
+    // where "temporary" variables (parameters in functions, p and q here.  
+    // HEAP memory is where dynamically allocated memory comes from.
+    // STACK and HEAP memory are shared.  If one gets too big, a STACK OVERFLOW
+    
+    // When p and q go out of SCOPE (where you can access the variable)
+    // stack variables go away.  The destructor gets called right before this
+    // happens.  You shouldn't call the destructor manually.
 }
 
